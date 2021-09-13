d1e0f874ee46292b2d02cb4af0aeebbfd388f3c1
Anne-Claire Coutoux <acoutoux@apperture.fr>
2021-09-13 16:17:43 +0200

    suite fin front

--- templates/my404.php
+++ templates/my404.php
@@ -3,12 +3,13 @@
 require_once __DIR__ . '/header.php'; ?>
 
 <!-- HTML -->
-<section id="pageErreur">
-    <div>
-        <p>Oups, impossible de trouver la page... vous pouvez rester ici ou <a href="http://localhost/Nouveau-projet/dashboard">revenir vers le droit chemin</a></p>
+<section id="pageErreur" style="background:black; height:100vh margin:0;">
+    <div class="error" role="dialog">
+        <p>Oups, impossible de trouver la page... vous pouvez rester ici ou <a href="http://localhost/Nouveau-projet/dashboard" class="btns btnswhite">revenir vers le droit chemin</a></p>
+        <p class="exit"><span class="exitLeft"></span><span class="exitRight"></span></p>
+    </div>
         <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
         <lottie-player src="https://assets5.lottiefiles.com/packages/lf20_wqwtneu4.json" background="#000000" speed="1" style="height: 80vh;" loop autoplay></lottie-player>
-    </div>
 </section>
 
 <?php
